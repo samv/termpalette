000c2093da77b9ac40a6161bda4172709b2137cc
Sam Vilain <sam@vilain.net>
2016-08-05 15:05:04 -0700

    Enable xterm support.
    I doubled these up for debugging.  This works with Xterm.

--- xterm.go
+++ xterm.go
@@ -9,12 +9,12 @@ import (
 
 func XtermPaletteCode(color Color, code ...int) []byte {
 	var escapeCode bytes.Buffer
-	escapeCode.WriteString("\\033]")
+	escapeCode.WriteString("\033]")
 	for _, c := range code {
 		escapeCode.WriteString(strconv.Itoa(c))
 		escapeCode.WriteRune(';')
 	}
-	escapeCode.WriteString(fmt.Sprintf("rgb:%.2x/%.2x/%.2x\\033\\", color[0], color[1], color[2]))
+	escapeCode.WriteString(fmt.Sprintf("rgb:%.2x/%.2x/%.2x\033\\", color[0], color[1], color[2]))
 	return escapeCode.Bytes()
 }
 
